033ac74043da6721b4249ba8619d983f0152e11f
Karen Gonzalez <karen.gonzalez01@iest.edu.mx>
2019-03-05 17:33:46 -0600

    sorpresea poo

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 module.exports = function tiny(string) {
-  //if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-  //return string.replace(/\s/g, "");
-
   const commands = [
-    { emoji: '🦄', name: 'unicorn' },
-    { emoji: '🍕', name: 'pizza' },
-    { emoji: '🍺', name: 'beer' },
-    { emoji: '💩', name: 'poo' }
-  ];
+        { emoji: '💩', name: 'poo' }
+    ];
 
-  // Create custom commands
-  commands.forEach(({ name, emoji }) => window.console[name] = (...args) => console.log(emoji + ' ' + args.join(', ')));
-  console.unicorn("So magical!");
+    commands.forEach(({ name, emoji }) => window.console[name] = (...args) => console.log(emoji + ' ' + args.join(', ')));
+    console.poo('Happy Poo!');
 };
