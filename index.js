625943991fecbd5dcb37f649024d07fdd0881d24
Karen Gonzalez <karen.gonzalez01@iest.edu.mx>
2019-03-05 17:04:43 -0600

    Some pretty things

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 module.exports = function tiny(string) {
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-  return string.replace(/\s/g, "");
+  //if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+  //return string.replace(/\s/g, "");
+
+  const commands = [
+    { emoji: '🦄', name: 'unicorn' },
+    { emoji: '🍕', name: 'pizza' },
+    { emoji: '🍺', name: 'beer' },
+    { emoji: '💩', name: 'poo' }
+  ];
+
+  // Create custom commands
+  commands.forEach(({ name, emoji }) => window.console[name] = (...args) => console.log(emoji + ' ' + args.join(', ')));
+  console.unicorn("So magical!");
 };
